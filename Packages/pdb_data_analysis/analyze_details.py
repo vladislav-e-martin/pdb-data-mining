ae7f496e1906944ac8da94f81551ca5d7e0ec86d
Vladislav Martin <vladislav.e.martin@gmail.com>
2015-06-29 14:30:38 -0400

    June 29th, 2015 - Fixed regex commands and added string processing commands in analyze_details.py to produce the names of chemicals used to crystallize the protein and their respective concentrations - Cleaned up the project files a bit

--- Packages/pdb_data_analysis/analyze_details.py
+++ Packages/pdb_data_analysis/analyze_details.py
@@ -11,68 +11,105 @@ from pprint import pprint
 
 # FUNCTION DECLARATIONS
 
-# Connect to the database
-def connect_database(folder):
-    connection = SQL.connect(folder)
-    return connection
 
+# no_space_matches = []
+# space_matches = []
+#
+# hyphen_separated_no_space_matches = []
+# hyphen_separated_space_matches = []
+#
+# peg_matches = []
 
-# SWITCH TO .FINDALL INSTEAD OF .SEARCH
-def find_common_chemicals(connection, table, column):
+# hyphen_space_search = re.findall('[\d+.\d]m{1,2}[ ][a-z]*[-][a-z]*', lowered_row)
+# hyphen_no_space_search = re.findall('[\d+.\d][ ]m{1,2}[ ][a-z]*[-][a-z]*', lowered_row)
+#
+# no_space_search = re.findall('[\d+.\d]m{1,2}[ ][^,.;)(\t]*', lowered_row)
+# space_search = re.findall('[\d+.\d][ ]m{1,2}[ ][^,.;)(\t]*', lowered_row)
+#
+# peg_search = re.search('(peg)', lowered_row)
+# found = False
+# # Find all of the entries that contain chemical names containing a hyphen where the concentration
+# # is not separated from it's unit by a space
+# for match in hyphen_no_space_search:
+#     if match is not None:
+#         hyphen_separated_no_space_matches.append(match)
+#         found = True
+# # Find all of the entries that contain chemical names containing a hyphen where the concentration
+# # is separated from it's unit by a space
+# for match in hyphen_space_search:
+#     if match is not None:
+#         hyphen_separated_space_matches.append(match)
+#         found = True
+# # Find all of the entries that contain chemical names where the concentration is not separated
+# # from it's unit by a space
+# for match in no_space_search:
+#     if match is not None:
+#         no_space_matches.append(match)
+#         found = True
+# # Find all of the entries that contain chemical names where the concentration is separated
+# # from it's unit by a space
+# for match in space_search:
+#     if match is not None:
+#         space_matches.append(match)
+#         found = True
+# if peg_search is not None and found == False:
+#     peg_matches.append(peg_search)
+#     text_file = open("/home/vlad/Documents/Code/pdb-data-mining/containing-peg.txt", "a")
+#     text_file.write("The row's contents are displayed below: \n {0}".format(row[6]))
+#     text_file.write("\n\n\n\n")
+#     text_file.close()
+
+def find_chemical_matches(connection, table, column):
     cursor = connection.cursor()
 
-    no_space_matches = []
-    space_matches = []
-
-    hyphen_separated_no_space_matches = []
-    hyphen_separated_space_matches = []
-
-    peg_matches = []
+    space_separated_results = []
+    not_space_separated_results = []
+    no_match_case_results = []
 
     sort_command = "SELECT * FROM {0} ORDER BY {1}".format(table, column)
     for index, row in enumerate(cursor.execute(sort_command)):
         lowered_row = row[6].lower()
-        hyphen_space_search = re.findall('[\d+.\d+]m*[ ][a-z]*[-][a-z]*', lowered_row)
-        hyphen_no_space_search = re.findall('[\d+.\d+][ ]m*[ ][a-z]*[-][a-z]*', lowered_row)
 
-        no_space_search = re.findall('[\d+.\d+]m*[ ]/([^,]+)/*', lowered_row)
-        space_search = re.findall('[\d+.\d+][ ]m*[ ]/([^,]+)/', lowered_row)
+        space_separated = re.findall('[\d+.\d]m{1,2}[ ][^,.;)(\t]*', lowered_row)
+        not_space_separated = re.findall('[\d+.\d]m{1,2}[ ][^,.;)(\t]*', lowered_row)
 
-        peg_search = re.search('(peg)', lowered_row)
-        found = False
-        # Find all of the entries that contain chemical names containing a hyphen where the concentration
-        # is not separated from it's unit by a space
-        for match in hyphen_no_space_search:
+        no_match_case = re.search('(peg)', lowered_row)
+
+        found_match = False
+        for match in space_separated:
             if match is not None:
-                hyphen_separated_no_space_matches.append(match)
-                found = True
-        # Find all of the entries that contain chemical names containing a hyphen where the concentration
-        # is separated from it's unit by a space
-        for match in hyphen_space_search:
+                # Remove the pH values from this matched string
+                match = re.sub('(ph{1}[ ][\d+.\d])', '', match)
+                # Remove leading and trailing whitespace from this matched string
+                match.strip()
+                space_separated_results.append(match)
+                found_match = True
+        for match in not_space_separated:
             if match is not None:
-                hyphen_separated_space_matches.append(match)
-                found = True
-        # Find all of the entries that contain chemical names where the concentration is not separated
-        # from it's unit by a space
-        for match in no_space_search:
-            if match is not None:
-                no_space_matches.append(match)
-                found = True
-        # Find all of the entries that contain chemical names where the concentration is separated
-        # from it's unit by a space
-        for match in space_search:
-            if match is not None:
-                space_matches.append(hyphen_space_search)
-                found = True
-        if peg_search is not None and found == False:
-            peg_matches.append(peg_search)
-            text_file = open("/home/vlad/Documents/Code/pdb-data-mining/containing-peg.txt", "a")
-            text_file.write("The row's contents are displayed below: \n {0}".format(row[6]))
-            text_file.write("\n\n\n\n")
-            text_file.close()
+                # Remove the pH values from this matched string
+                match = re.sub('(ph{1}[ ][\d+.\d])', '', match)
+                # Remove leading and trailing whitespace from this matched string
+                match.strip()
+                not_space_separated_results.append(match)
+                found_match = True
+
+        if no_match_case is not None and not found_match:
+            no_match_case_results.append(no_match_case)
+
+    return space_separated_results, not_space_separated_results, no_match_case_results
+
+
+def store_results(connection, table, results):
+    cursor = connection.cursor()
+
+
+def calculate_frequency_of_chemical_names(connection, table, column):
+    cursor = connection.cursor()
 
     # Create the child table that will store the chemicals and their respective concentrations that were used to grow
     # the crystallized protein structures
     M_SQL.add_table(connection, "chemicals")
 
-    return hyphen_separated_no_space_matches, hyphen_separated_space_matches, no_space_matches, space_matches, peg_matches
+    frequency_command = "SELECT {1}, COUNT(*) FROM {0} GROUP BY {1}".format(table, column)
+
+    cursor.execute(frequency_command)
--- main.py
+++ main.py
@@ -25,45 +25,61 @@ XML_NO_ATOM = "/pub/pdb/data/structures/divided/XML-noatom/"
 TEXT = "/pub/pdb/data/structures/divided/pdb/"
 
 # For parsing contents of files
-COLUMNS = 9
-DB_COLUMNS = 7
+RAW_COLUMNS = 9
+SORTED_COLUMNS = 7
 
 # For managing SQL database
 BASE_DB = "/home/vlad/Documents/Code/pdb-data-mining/Database/information.db"
 
 # MAIN BODY
 
-# ftp_conn = FTP.connect(XML_NO_ATOM)
-# archive_count = FTP.count_archives(ftp_conn)
-# pprint("There are a total of {0} directories in the FTP site!".format(archive_count))
-# pprint("To divide this into 4 separate processes: {0}".format(archive_count / 4))
-# start = input("Where would you like to start downloading: ")
-# start_point = int(start)
-# end = input("Where would you like to end downloading: ")
-# end_point = int(end)
-# FTP.download(BASE_XML_NO_ATOM, ftp_conn, start_point, end_point)
-# FTP.disconnect(ftp_conn)
+# Download structures from the FTP site
+def ftp_download(ftp_base, local_base):
+    ftp_conn = FTP.connect(ftp_base)
+    archive_count = FTP.count_archives(ftp_conn)
+    # Provide the user with information to split the downloading workload between several processes
+    pprint("There are a total of {0} directories in the FTP site!".format(archive_count))
+    # Specifically, 4 processes
+    pprint("To divide this into 4 separate processes: {0}".format(archive_count / 4))
+    # Ask the user at which directory the downloading process should begin
+    start = input("Where would you like to start downloading: ")
+    start_point = int(start)
+    # Ask the user at which directory the downloading process should end
+    end = input("Where would you like to end downloading: ")
+    end_point = int(end)
+    # Commence the downloading process
+    FTP.download(local_base, ftp_conn, start_point, end_point)
+    # Once all of the files have been downloaded from the specified section of the data bank, disconnect from the site
+    FTP.disconnect(ftp_conn)
 
-# files = XML.fill_list(BASE_XML_NO_ATOM)
-# raw_data = XML.fill_raw(files, COLUMNS)
-# sorted_indices = XML.filter_indices(raw_data)
-# sorted_data = XML.fill_sorted(raw_data, sorted_indices, DB_COLUMNS)
-#
-# # Create a new database
-# connection = SQL.connect_database(BASE_DB)
-# # Add a new table to the database
-# SQL.add_table(connection, "information")
-# # Insert each row of values from sorted_data
-# SQL.add_row(connection, "information", sorted_data)
-# # Print the contents of the database as confirmation
-# SQL.print_database(connection, "information", "id")
-# # Commit changes and disconnect from the database
-# SQL.commit_changes(connection)
 
-new_connection = ANALYZE.connect_database(BASE_DB)
-matches = ANALYZE.find_common_chemicals(new_connection, "information", "id")
+# Parse through all of the newly downloaded structures
+def parse_raw_data(local_base, raw_columns, sorted_columns) -> object:
+    files = XML.fill_list(local_base)
+    raw_data = XML.fill_raw(files, raw_columns)
+    sorted_indices = XML.filter_indices(raw_data)
+    sorted_data = XML.fill_sorted(raw_data, sorted_indices, sorted_columns)
 
-pprint("Hyphen-separated (no space): {0}, Hyphen-separated (space): {1}, No space: {2}, Space: {3}".format(matches[0],
-                                                                                                           matches[1],
-                                                                                                           matches[2],
-                                                                                                           matches[3]))
+    return sorted_data
+
+
+# Store the important structures in the database for further analysis
+def store_in_database(local_base, sorted_data):
+    connection = SQL.connect_database(local_base)
+    # Add a new table to the database
+    SQL.add_table(connection, "information")
+    # Insert each row of values from sorted_data
+    SQL.add_row(connection, "information", sorted_data)
+    # Print the contents of the database as confirmation
+    SQL.print_database(connection, "information", "id")
+    # Commit changes and disconnect from the database
+    SQL.commit_changes(connection)
+
+
+connection = SQL.connect_database(BASE_DB)
+matches = ANALYZE.find_chemical_matches(connection, "information", "id")
+
+pprint("Space separated: {0}, Not space separated: {1}, No match case: {2}".format(len(matches[0]),
+                                                                                   len(matches[1]),
+                                                                                   len(matches[2])))
+pprint("Space values: {0}".format(matches[0]))
