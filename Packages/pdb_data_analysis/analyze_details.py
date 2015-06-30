5553da3f62f3daa15a8e88bc00ff23f236c22aa9
Vladislav Martin <vladislav.e.martin@gmail.com>
2015-06-30 00:32:48 -0400

    June 29th, 2015 - Cleaned up the code in main.py, analyze_details.py, and manage_sql.py so that code will be easier to read and final edits will be easier to make - Still have yet to successfully store chemical data in the child table or discover the frequency of each chemical's name; that is the goal for June 30th, 2015

--- Packages/pdb_data_analysis/analyze_details.py
+++ Packages/pdb_data_analysis/analyze_details.py
@@ -20,103 +20,75 @@ from pprint import pprint
 
 # Need to keep track of the associated entry_id as well
 def find_chemical_matches(connection, table, column):
-    cursor = connection.cursor()
+	cursor = connection.cursor()
 
-    space_separated_results = []
-    not_space_separated_results = []
-    no_match_case_results = []
+	space_separated_results = []
+	not_space_separated_results = []
+	no_match_case_results = []
 
-    sort_command = "SELECT * FROM {0} ORDER BY {1}".format(table, column)
-    for index, row in enumerate(cursor.execute(sort_command)):
-        lowered_row = row[6].lower()
+	sort_command = "SELECT * FROM {0} ORDER BY {1}".format(table, column)
+	for index, row in enumerate(cursor.execute(sort_command)):
+		lowered_row = row[6].lower()
 
-        space_separated = re.findall('[\d+][ ]m{1,2}[ ][^,.;)(\t]*', lowered_row)
-        not_space_separated = re.findall('[\d+]m{1,2}[ ][^,.;)(\t]*', lowered_row)
+		space_separated = re.findall('[\d+][ ]m{1,2}[ ][^,.;)(\t]*', lowered_row)
+		not_space_separated = re.findall('[\d+]m{1,2}[ ][^,.;)(\t]*', lowered_row)
 
-        no_match_case = re.search('(peg)', lowered_row)
+		no_match_case = re.search('(peg)', lowered_row)
 
-        found_match = False
-        for match in space_separated:
-            if match is not None:
-                # Remove the pH values from this matched string
-                new_match = re.sub('(ph[ ][\d+])', '', match)
-                re.sub('(ph\d+)', '', new_match)
-                # Remove leading and trailing whitespace from this matched string
-                new_match.strip()
-                space_separated_results.append(new_match)
-                found_match = True
-        for match in not_space_separated:
-            if match is not None:
-                # Remove the pH values from this matched string
-                new_match = re.sub('(ph[ ][\d+])', '', match)
-                re.sub('(ph\d+)', '', new_match)
-                # Remove leading and trailing whitespace from this matched string
-                new_match.strip()
-                not_space_separated_results.append(new_match)
-                found_match = True
+		found_match = False
+		for match in space_separated:
+			if match is not None:
+				# Remove the pH values from this matched string
+				new_match = re.sub('(ph[ ][\d+])', '', match)
+				re.sub('(ph\d+)', '', new_match)
+				# Remove leading and trailing whitespace from this matched string
+				new_match.strip()
+				space_separated_results.append(new_match)
+				found_match = True
+		for match in not_space_separated:
+			if match is not None:
+				# Remove the pH values from this matched string
+				new_match = re.sub('(ph[ ][\d+])', '', match)
+				re.sub('(ph\d+)', '', new_match)
+				# Remove leading and trailing whitespace from this matched string
+				new_match.strip()
+				not_space_separated_results.append(new_match)
+				found_match = True
 
-        if no_match_case is not None and not found_match:
-            no_match_case_results.append(no_match_case)
+		if no_match_case is not None and not found_match:
+			no_match_case_results.append(no_match_case)
 
-    merged_results = space_separated_results + not_space_separated_results
+	merged_results = space_separated_results + not_space_separated_results
 
-    return merged_results
+	return merged_results
 
 
 def split_results(results):
-    rows = len(results)
-    result_components = [[0 for x in range(3)] for x in range(rows)]
-    for index, chemical in enumerate(results):
-        concentration_search = re.compile('(\d+)\s*(\w+)')
-        pprint("The whole string: {0}".format(chemical))
-        concentration = concentration_search.match(chemical).group(1)
-        unit = concentration_search.match(chemical).group(2)
-        pprint("Matches: {0} and {1}".format(concentration, unit))
-        result_components[index][0] = concentration
-        result_components[index][1] = unit
-        name = re.sub(concentration, '', chemical, 1)
-        new_name = re.sub(unit, '', name, 1)
-        s_name = new_name.strip()
-        result_components[index][2] = s_name
-
-        pprint("Reduced string: {0}".format(s_name))
-    return result_components
-    # result_components[index][0] = value_match.group(1)
-    # result_components[index][1] = value_match.group(2)
-    # pprint("Concentration: {0}".format(result_components[index][0]))
-    # pprint("Concentration's unit: {0}".format(result_components[index][1]))
-
-    # concentration_matches = re.compile('([\d+.\d])')
-    # concentration_match = concentration_matches.search(chemical)
-    # chemical.replace(concentration_match.group(1), '')
-    # result_components[index][0] = concentration_match.group(1)
-    # pprint("Concentration: {0}".format(result_components[index][0]))
-    # pprint("New chemical string: {0}".format(chemical))
-    #
-    # unit_matches = re.compile('(m+)')
-    # unit_match = unit_matches.search(chemical)
-    # chemical.replace(unit_match.group(1), '')
-    # result_components[index][1] = unit_match.group(1)
-    # pprint("Concentration's unit: {0}".format(result_components[index][1]))
-    # pprint("New chemical string: {0}".format(chemical))
-    # name = chemical
-    # result_components[index][2] = name
-
-    # for index in range(len(result_components)):
-    #     pprint("The current index: {0}".format(index))
-    #     pprint("The concentration: {0}, The concentration's unit: {1}, The chemical's name: {2}"
-    #            .format(result_components[index][0],
-    #                    result_components[index][1],
-    #                    result_components[index][2]))
+	rows = len(results)
+	result_components = [[0 for x in range(3)] for x in range(rows)]
+	for index, chemical in enumerate(results):
+		concentration_search = re.compile('(\d+)\s*(\w+)')
+		pprint("The whole string: {0}".format(chemical))
+		concentration = concentration_search.match(chemical).group(1)
+		unit = concentration_search.match(chemical).group(2)
+		result_components[index][0] = concentration
+		result_components[index][1] = unit
+		original_string = re.sub(concentration, '', chemical, 1)
+		no_concentration_string = re.sub(unit, '', original_string, 1)
+		name = no_concentration_string.strip()
+		result_components[index][2] = name
+	return result_components
 
 
+# NOT COMPLETE
 def store_results(connection, table, results):
-    M_SQL.add_table(connection, table)
-    M_SQL.add_child_row(connection, table, results)
+	M_SQL.add_table(connection, table)
+	M_SQL.add_child_row(connection, table, results)
 
+
+# NOT COMPLETE
 def calculate_frequency_of_chemical_names(connection, table, column):
-    cursor = connection.cursor()
+	cursor = connection.cursor()
 
-    frequency_command = "SELECT {1}, COUNT(*) FROM {0} GROUP BY {1}".format(table, column)
-
-    pprint(cursor.execute(frequency_command))
+	frequency_command = "SELECT {1}, COUNT(*) FROM {0} GROUP BY {1}".format(table, column)
+	cursor.execute(frequency_command)
--- main.py
+++ main.py
@@ -29,54 +29,66 @@ RAW_COLUMNS = 9
 SORTED_COLUMNS = 7
 
 # For managing SQL database
-BASE_DB = "/home/vlad/Documents/Code/pdb-data-mining/Database/information.db"
+PARENT_BASE_DB = "/home/vlad/Documents/Code/pdb-data-mining/Database/information.db"
+CHILD_BASE_DB = "/home/vlad/Documents/Code/pdb-data-mining/Database/crystallization_chemicals.db"
 
 # MAIN BODY
 
 # Download structures from the FTP site
 def ftp_download(ftp_base, local_base):
-    ftp_conn = FTP.connect(ftp_base)
-    archive_count = FTP.count_archives(ftp_conn)
-    # Provide the user with information to split the downloading workload between several processes
-    pprint("There are a total of {0} directories in the FTP site!".format(archive_count))
-    # Specifically, 4 processes
-    pprint("To divide this into 4 separate processes: {0}".format(archive_count / 4))
-    # Ask the user at which directory the downloading process should begin
-    start = input("Where would you like to start downloading: ")
-    start_point = int(start)
-    # Ask the user at which directory the downloading process should end
-    end = input("Where would you like to end downloading: ")
-    end_point = int(end)
-    # Commence the downloading process
-    FTP.download(local_base, ftp_conn, start_point, end_point)
-    # Once all of the files have been downloaded from the specified section of the data bank, disconnect from the site
-    FTP.disconnect(ftp_conn)
+	ftp_conn = FTP.connect(ftp_base)
+	archive_count = FTP.count_archives(ftp_conn)
+	# Provide the user with information to split the downloading workload between several processes
+	pprint("There are a total of {0} directories in the FTP site!".format(archive_count))
+	# Specifically, 4 processes
+	pprint("To divide this into 4 separate processes: {0}".format(archive_count / 4))
+	# Ask the user at which directory the downloading process should begin
+	start = input("Where would you like to start downloading: ")
+	start_point = int(start)
+	# Ask the user at which directory the downloading process should end
+	end = input("Where would you like to end downloading: ")
+	end_point = int(end)
+	# Commence the downloading process
+	FTP.download(local_base, ftp_conn, start_point, end_point)
+	# Once all of the files have been downloaded from the specified section of the data bank, disconnect from the site
+	FTP.disconnect(ftp_conn)
+
 
 # Parse through all of the newly downloaded structures
 def parse_raw_data(local_base, raw_columns, sorted_columns) -> object:
-    files = XML.fill_list(local_base)
-    raw_data = XML.fill_raw(files, raw_columns)
-    sorted_indices = XML.filter_indices(raw_data)
-    sorted_data = XML.fill_sorted(raw_data, sorted_indices, sorted_columns)
+	files = XML.fill_list(local_base)
+	raw_data = XML.fill_raw(files, raw_columns)
+	sorted_indices = XML.filter_indices(raw_data)
+	sorted_data = XML.fill_sorted(raw_data, sorted_indices, sorted_columns)
 
-    return sorted_data
+	return sorted_data
+
 
 # Store the important structures in the database for further analysis
-def store_in_database(local_base, sorted_data):
-    connection = SQL.connect_database(local_base)
-    # Add a new table to the database
-    SQL.add_table(connection, "information")
-    # Insert each row of values from sorted_data
-    SQL.add_parent_row(connection, "information", sorted_data)
-    # Print the contents of the database as confirmation
-    SQL.print_database(connection, "information", "id")
-    # Commit changes and disconnect from the database
-    SQL.commit_changes(connection)
+def store_in_information_table(local_base, sorted_data):
+	connection = SQL.connect_database(local_base)
+	# Add a new table to the database
+	SQL.add_table(connection, "information")
+	# Insert each row of values from sorted_data
+	SQL.add_parent_row(connection, "information", sorted_data)
+	# Print the contents of the database as confirmation
+	SQL.print_database(connection, "information", "id")
+	# Commit changes and disconnect from the database
+	SQL.commit_changes(connection)
 
-connection = SQL.connect_database(BASE_DB)
-matches = ANALYZE.find_chemical_matches(connection, "information", "id")
-split_matches = ANALYZE.split_results(matches)
-ANALYZE.store_results(connection, "test_chemicals", split_matches)
-ANALYZE.calculate_frequency_of_chemical_names(connection, "test_chemicals", "name")
 
-# pprint("All matches: {0}".format(matches))
+def store_in_chemicals_table(local_parent_base, local_child_base):
+	parent_connection = SQL.connect_database(local_parent_base)
+	child_connection = SQL.connect_database(local_child_base)
+	# Find all of the chemicals stored in the crystallization information of the structures
+	matches = ANALYZE.find_chemical_matches(parent_connection, "information", "id")
+	# Split the chemicals into their concentrations, the units used to measure concentration,
+	# and the name of the chemical itself
+	split_matches = ANALYZE.split_results(matches)
+	# Store the results of the split_matches() function into a child table in the database
+	ANALYZE.store_results(child_connection, "test_chemicals", split_matches)
+	# Display the frequency of each chemical name stored in the child table
+	ANALYZE.calculate_frequency_of_chemical_names(child_connection, "test_chemicals", "name")
+
+
+store_in_chemicals_table(PARENT_BASE_DB, CHILD_BASE_DB)
