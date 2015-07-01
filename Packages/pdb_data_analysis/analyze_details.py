b9673b7e5887862d041197cbe1a00bf3da7b4aed
Vladislav Martin <vladislav.e.martin@gmail.com>
2015-07-01 17:20:59 -0400

    July 1st, 2015 - Further refined the regex commands to find only true chemical names (there are still some unfortunate formatting errors on the part of the depositors) - Completed the non-ionic list of chemicals - Will begin designing a search to produce all protein structures that were crystallized with ONLY non-ionic chemicals contained in the above-mentioned list

--- Packages/pdb_data_analysis/analyze_details.py
+++ Packages/pdb_data_analysis/analyze_details.py
@@ -12,116 +12,113 @@ from pprint import pprint
 
 # FUNCTION DECLARATIONS
 
-# if peg_search is not None and found == False:
-#     peg_matches.append(peg_search)
-#     text_file = open("/home/vlad/Documents/Code/pdb-data-mining/containing-peg.txt", "a")
-#     text_file.write("The row's contents are displayed below: \n {0}".format(row[6]))
-#     text_file.write("\n\n\n\n")
-#     text_file.close()
-
 # Need to keep track of the associated entry_id as well
 def find_chemical_matches(connection, table, column):
-	cursor = connection.cursor()
+    cursor = connection.cursor()
 
-	sort_command = "SELECT * FROM {0} ORDER BY {1}".format(table, column)
+    sort_command = "SELECT * FROM {0} ORDER BY {1}".format(table, column)
 
-	space_separated_results = []
-	space_separated_id = []
-	not_space_separated_results = []
-	not_space_separated_id = []
+    space_separated_results = []
+    space_separated_id = []
+    not_space_separated_results = []
+    not_space_separated_id = []
 
-	for index, row in enumerate(cursor.execute(sort_command)):
-		entry_id = row[0]
-		details = row[6].lower()
+    for index, row in enumerate(cursor.execute(sort_command)):
+        entry_id = row[0]
+        details = row[6].lower()
 
-		space_separated = re.findall('[\d+][ ]m{1,2}[ ][^,.;:\t(at)(in)(and)]*', details)
-		not_space_separated = re.findall('[\d+]m{1,2}[ ][^,.;:\t(at)(in)(and)]*', details)
+        space_separated = re.findall('[\d+][ ]m{1,2}[ ][^,.;:\t]*', details)
+        not_space_separated = re.findall('[\d+]m{1,2}[ ][^,.;:\t]*', details)
 
-		found_match = False
-		for match in space_separated:
-			if match is not None:
-				# Remove the pH values from this matched string
-				trimmed_match = re.sub('(ph[ ][\d+])', '', match)
-				alt_trimmed_match = re.sub('(ph\d+)', '', trimmed_match)
-				# Remove leading and trailing whitespace from this matched string
-				chemical = alt_trimmed_match.strip()
-				space_separated_results.append(chemical)
-				space_separated_id.append(entry_id)
-				found_match = True
-		for match in not_space_separated:
-			if match is not None:
-				# Remove the pH values from this matched string
-				trimmed_match = re.sub('(ph[ ][\d+])', '', match)
-				alt_trimmed_match = re.sub('(ph\d+)', '', trimmed_match)
-				# Remove leading and trailing whitespace from this matched string
-				chemical = alt_trimmed_match.strip()
-				not_space_separated_results.append(chemical)
-				not_space_separated_id.append(entry_id)
-				found_match = True
+        found_match = False
+        for match in space_separated:
+            # [\d+][ ](ph)
+            if match is not None:
+                # Remove the English language delimiters and pH values from this matched string
+                trim_english_match = re.sub('[ ](and)[ ] | [ ](at)[ ] | [ ](in)[ ] | (buffer)', '', match)
+                trimmed_match = re.sub('(ph)[ ][\d+]', '', trim_english_match)
+                alt_trimmed_match = re.sub('(ph\d+)', '', trimmed_match)
+                # Remove leading and trailing whitespace from this matched string
+                chemical = alt_trimmed_match.strip()
+                space_separated_results.append(chemical)
+                space_separated_id.append(entry_id)
+                found_match = True
+        for match in not_space_separated:
+            # [\d+][ ](ph)
+            if match is not None:
+                # Remove the English language delimiters and pH values from this matched string
+                trim_english_match = re.sub('[ ](and)[ ] | [ ](at)[ ] | [ ](in)[ ] | (buffer)', '', match)
+                trimmed_match = re.sub('(ph)[ ][\d+]', '', trim_english_match)
+                alt_trimmed_match = re.sub('(ph\d+)', '', trimmed_match)
+                # Remove leading and trailing whitespace from this matched string
+                chemical = alt_trimmed_match.strip()
+                not_space_separated_results.append(chemical)
+                not_space_separated_id.append(entry_id)
+                found_match = True
 
-	merged_results = space_separated_results + not_space_separated_results
-	merged_ids = space_separated_id + not_space_separated_id
-	return merged_results, merged_ids
+    merged_results = space_separated_results + not_space_separated_results
+    merged_ids = space_separated_id + not_space_separated_id
+    return merged_results, merged_ids
 
 
 def find_custom_crystallization_conditions(connection, table, column, include_chemicals, exclude_chemicals):
-	cursor = connection.cursor()
+    cursor = connection.cursor()
 
-	sort_command = "SELECT * FROM {0} ORDER BY {1}".format(table, column)
+    sort_command = "SELECT * FROM {0} ORDER BY {1}".format(table, column)
 
-	found_chemicals = []
+    found_chemicals = []
 
-	for index, row in enumerate(cursor.execute(sort_command)):
-		entry_id = row[0]
-		details = row[6].lower()
+    for index, row in enumerate(cursor.execute(sort_command)):
+        entry_id = row[0]
+        details = row[6].lower()
 
-		# ammonium_sulfate_search = re.findall('(ammonium sulfate)', details)
-		# ammonium_sulphate_search = re.findall('(ammonium sulphate)', details)
-		# nh4_so4_search = re.findall('(nh4so4)', details)
-		# peg_search = re.findall('(peg)', details)
+        # ammonium_sulfate_search = re.findall('(ammonium sulfate)', details)
+        # ammonium_sulphate_search = re.findall('(ammonium sulphate)', details)
+        # nh4_so4_search = re.findall('(nh4so4)', details)
+        # peg_search = re.findall('(peg)', details)
 
-		for chemical_name in include_chemicals:
-			if chemical_name in row:
-				found_chemicals.append(row[6])
+        for chemical_name in include_chemicals:
+            if chemical_name in row:
+                found_chemicals.append(row[6])
 
-	for chemical_name in exclude_chemicals:
-		if chemical_name in found_chemicals:
-			pass
-		# found_chemicals.remove(current_item)
+    for chemical_name in exclude_chemicals:
+        if chemical_name in found_chemicals:
+            pass
+            # found_chemicals.remove(current_item)
 
 
 def split_results(results, result_ids):
-	rows = len(results)
-	result_components = [[0 for x in range(5)] for x in range(rows)]
-	for index, chemical in enumerate(results):
-		result_components[index][0] = str(uuid.uuid4())
-		result_components[index][1] = result_ids[index]
-		concentration_search = re.compile('(\d+)\s*(\w+)')
-		concentration = concentration_search.match(chemical).group(1)
-		unit = concentration_search.match(chemical).group(2)
-		result_components[index][2] = concentration
-		result_components[index][3] = unit
-		original_string = re.sub(concentration, '', chemical, 1)
-		no_concentration_string = re.sub(unit, '', original_string, 1)
-		name = no_concentration_string.strip()
-		result_components[index][4] = name
-	return result_components
+    rows = len(results)
+    result_components = [[0 for x in range(5)] for x in range(rows)]
+    for index, chemical in enumerate(results):
+        result_components[index][0] = str(uuid.uuid4())
+        result_components[index][1] = result_ids[index]
+        concentration_search = re.compile('(\d+)\s*(\w+)')
+        concentration = concentration_search.match(chemical).group(1)
+        unit = concentration_search.match(chemical).group(2)
+        result_components[index][2] = concentration
+        result_components[index][3] = unit
+        original_string = re.sub(concentration, '', chemical, 1)
+        no_concentration_string = re.sub(unit, '', original_string, 1)
+        name = no_concentration_string.strip()
+        result_components[index][4] = name
+    return result_components
 
 
 # Comments here, complete
 def store_results(connection, table, results):
-	M_SQL.add_table(connection, table, False, True)
-	M_SQL.add_child_row(connection, table, results)
+    M_SQL.add_table(connection, table, False, True)
+    M_SQL.add_child_row(connection, table, results)
 
 
 # Comments here, complete
 def create_common_chemical_names_list(connection, table, column):
-	cursor = connection.cursor()
+    cursor = connection.cursor()
 
-	frequency_command = "SELECT COUNT(*), {1} FROM {0} GROUP BY {1} ORDER BY 1 DESC LIMIT 200".format(table, column)
-	file = open("/home/vlad/Documents/Code/pdb-data-mining/"
-	            "Results of Analysis/most-common-crystallization-chemicals.txt", 'w')
-	for index, row in enumerate(cursor.execute(frequency_command)):
-		file.write("{0}. \n".format(index))
-		file.write("{1} occurred {0} times \n\n".format(row[0], row[1]))
-	file.close()
+    frequency_command = "SELECT COUNT(*), {1} FROM {0} GROUP BY {1} ORDER BY 1 DESC LIMIT 250".format(table, column)
+    file = open("/home/vlad/Documents/Code/pdb-data-mining/"
+                "Results of Analysis/most-common-crystallization-chemicals.txt", 'w')
+    for index, row in enumerate(cursor.execute(frequency_command)):
+        file.write("{0}. \n".format(index))
+        file.write("{1} occurred {0} times \n\n".format(row[0], row[1]))
+    file.close()
--- main.py
+++ main.py
@@ -32,100 +32,7 @@ BASE_DB = "/home/vlad/Documents/Code/pdb-data-mining/Database/information.db"
 
 # For chemical name searches
 non_ionic = ["peg", "dtt", "edta", "tcep", "bme", "dithiothreitol", "nan3", "atp", "beta-mercaptoethanol", "kscn",
-             "2-mercaptoethanol", "gsh", "ada", "adp", "ddt", "nad+"]
-ammonium_sulfate = ["ammonium sulfate", "ammonium_sulphate", "amso4", "nh4so4"]
-hepes = ["hepes", "hepes-naoh", "hepes/naoh", "hepes buffer", "hepes-na", "na-hepes", "na hepes", "sodium hepes",
-         "hepes sodium", "hepes 7"]
-tris = ["tris", "tris-hcl", "bis-tris", "bis-tris propane", "tris hcl", "tris/hcl", "tris-cl", "bistris", "trishcl",
-        "tris buffer", "tris hydrochloride", "tris-hcl buffer", "bis-tris-propane", "bis-tris buffer",
-        "bis tris propane", "bis tris"]
-mes = ["mes", "mes buffer", "mes-naoh", "mes monohydrate"]
-sodium_chloride = ["nacl", "sodium chloride"]
-sodium_acetate = ["sodium acetate", "na acetate", "sodium acetate trihydrate", "na-acetate", "naoac", "naacetate",
-                  "sodium acetate buffer", "naac"]
-magnesium_chloride = ["mgcl2", "magnesium chloride", "magnesium chloride hexahydrate", "mgcl"]
-sodium_citrate = ["sodium citrate", "na citrate", "tri-sodium citrate", "na-citrate", "nacitrate", "tri-sodium citrate",
-                  "trisodium citrate", "sodium citrate tribasic dihydrate", "sodium citrate buffer"]
-ammonium_acetate = ["ammonium acetate", "nh4ac", "nh4 acetate"]
-sodium_cacodylate = ["sodium cacodylate", "na cacodylate", "sodium cacodylate trihydrate", "na-cacodylate",
-                     "nacacodylate"]
-lithium_sulfate = ["lithium sulfate", "li2so4", "lithium sulphate", "lithium sulfate monohydrate"]
-calcium_chloride = ["cacl2", "calcium chloride", "calcium chloride dihydrate"]
-imidazole = ["imidazole"]
-sodium_formate = ["sodium formate", "na formate"]
-calcium_acetate = ["calcium acetate", "ca acetate"]
-potassium_phosphate = ["potassium phosphate"]
-potassium_chloride = ["kcl", "potassium chloride"]
-sodium_phosphate = ["sodium phosphate", "sodium dihydrogen phosphate", "sodium phosphate buffer"]
-citrate = ["citrate", "citrate buffer"]
-cacodylate = ["cacodylate", "cacodylate buffer"]
-sodium_malonate = ["sodium malonate"]
-magnesium_acetate = ["magnesium acetate", "mg acetate"]
-bicine = ["bicine"]
-citric_acid = ["citric acid"]
-lithium_chloride = ["licl", "lithium chloride"]
-zinc_acetate = ["zinc acetate"]
-liso4 = ["liso4"]
-acetate = ["acetate", "acetate buffer"]
-ches = ["ches"]
-sodium_potassium_phosphate = ["na/k phosphate", "sodium/potassium phosphate", "sodium-potassium phosphate"]
-ammonium_citrate = ["ammonium citrate"]
-manganese_chloride = ["mncl2", "manganese chloride"]
-magnesium_sulfate = ["mgso4", "magnesium sulfate"]
-ammonium_phosphate = ["ammonium phosphate", "ammonium dihydrogen phosphate", "ammonium phosphate monobasic"]
-phosphate = ["phosphate buffer", "phosphate"]
-phosphate_citrate = ["phosphate-citrate", "phosphate citrate"]
-mops = ["mops"]
-k2hpo4 = ["k2hpo4"]
-glycine = ["glycine"]
-magnesium_formate = ["magnesium formate"]
-pipes = ["pipes"]
-zinc_chloride = ["zncl2", "zinc chloride"]
-kh2po4 = ["kh2po4"]
-ammonium_chloride = ["ammonium chloride", "nh4cl"]
-zinc_sulfate = ["znso4", "zinc sulfate"]
-nah2po4 = ["nah2po4"]
-ammonium_formate = ["ammonium formate"]
-potassium_thiocyanate = ["potassium thiocyanate"]
-succinic_acid = ["succinic327 acid"]
-dl_malic_acid = ["dl-malic acid"]
-potassium_sodium_tartrate = ["potassium sodium tartrate", "k/na tartrate", "na/k tartrate",
-                             "potassium sodium tartrate tetrahydrate", "sodium potassium tartrate"]
-btp = ["btp"]
-nickel_chloride = ["nicl2"]
-magnesium = ["mg"]
-sodium_nitrate = ["sodium nitrate", "nano3"]
-cdcl2 = ["cdcl2"]
-sodium_fluoride = ["naf", "sodium fluoride"]
-sodium_iodide = ["nai", "sodium iodide"]
-caps = ["caps"]
-ammonium_nitrate = ["ammonium nitrate"]
-sodium_sulfate = ["sodium sulfate", "na2so4"]
-sodium_azide = ["sodium azide"]
-potassium_acetate = ["potassium acetate"]
-spermine = ["spermine"]
-sodium = ["na"]
-nh4oac = ["nh4oac"]
-sodium_dithionite = ["sodium dithionite"]
-cadmium_chloride = ["cadmium chloride"]
-sodium_thiocyanate = ["sodium thiocyanate"]
-sodium_bromide = ["sodium bromide", "nabr"]
-cacodylic_acid = ["cacodylic acid"]
-ammonium_iodide = ["ammonium iodide"]
-potassium_nitrate = ["potassium nitrate"]
-bes = ["bes", "bes buffer"]
-cesium_chloride = ["cscl"]
-hexaamminecobalt = ["hexaamminecobalt"]
-nh4h2po4 = ["nh4h2po4"]
-oxonate = ["oxonate"]
-potassium_formate = ["potassium formate"]
-cocl2 = ["cocl2"]
-ammonium_tartrate = ["ammonium tartrate"]
-napi = ["napi"]
-sodium_tartrate = ["sodium tartrate"]
-ammonium_fluoride = ["ammonium fluoride"]
-cobalt_chloride = ["cobalt chloride"]
-magnesium_nitrate = ["magnesium nitrate"]
+             "2-mercaptoethanol", "gsh", "ada", "adp", "ddt", "nad+", "l-cysteine", "nascn", "mib", "l-proline", "nadp"]
 
 # MAIN BODY
 
@@ -189,7 +96,7 @@ def store_in_chemicals_table(local_base):
 def custom_search(local_base):
 	connection = SQL.connect_database(local_base)
 	# Find all of the chemicals stored in the crystallization information of the structures
-	matches = ANALYZE.find_custom_crystallization_conditions(connection, "information", "id", non_ionic, ionic)
+# matches = ANALYZE.find_custom_crystallization_conditions(connection, "information", "id", non_ionic, ionic)
 
 
 store_in_chemicals_table(BASE_DB)
